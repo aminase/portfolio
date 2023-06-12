0c2f9d1f7ca3e599331031cce61662a5fa15361c
Amina Selimovic <amina.selimovic@bancstac.com>
2023-06-12 19:02:50 +0200

    Added path for favicon

---
+++ .history/src/pages/_app_20230612190213.js
@@ -0,0 +1,13 @@
+import Theme from '../styles/theme'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <link rel='icon' href='/favicon.ico' sizes='32x32' />
+      <title>Portfolio | Amina Selimovic</title>
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+    </>
+  )
+}
