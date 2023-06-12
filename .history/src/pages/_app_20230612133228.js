e5caac794af6aa71fb1a09afd7f3e01f6da7779d
Amina Selimovic <amina.selimovic@bancstac.com>
2023-06-12 14:31:04 +0200

    Added another technology

--- .history/src/pages/_app_20230612133228.js
+++ .history/src/pages/_app_20230612133228.js
@@ -3,7 +3,7 @@ import Theme from '../styles/theme'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <link rel='icon' href='/favicon.ico' sizes='32x32' />
+      <link rel='icon' href='../../public/favicon.ico' sizes='32x32' />
       <title>Portfolio | Amina Selimovic</title>
       <Theme>
         <Component {...pageProps} />
